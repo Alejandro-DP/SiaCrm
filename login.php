0e65f78910049c814a6b1ab7ef2150167a2f5f0e
Alejandro-DP <alexdelacruz1888@gmail.com>
2021-07-19 14:15:42 -0600

    pr

--- login.php
+++ login.php
@@ -24,50 +24,7 @@
 	   		<input class="btn btn-lg btn-primary btn-block" id="btn" type="submit" name="enviar" value="Accesar">
         </div>
 	</form> -->
-    <body class="my-login-page">
-	<section class="h-100">
-		<div class="container h-100">
-			<div class="row justify-content-md-center h-100">
-				<div class="card-wrapper">
-					<div class="brand">
-						<img src="https://static.wixstatic.com/media/4fddf6_6e0b5a629f9d4fa289ec266ca6bbecd4~mv2.png/v1/fill/w_79,h_79,al_c,q_85,usm_0.66_1.00_0.01/logoinferior.webp" alt="logo">
-					</div>
-					<div class="card fat">
-						<div class="card-body">
-							<h4 class="card-title">SIA</h4>
-							<form action="/SiaCrm/services/validar.php" method="POST" >
-								<div class="form-group">
-									<label for="email">Usuario</label>
-									<input id="email" type="txt" class="form-control" name="user" value="" >
-									
-								</div>
-
-								<div class="form-group">
-									<label for="password">Constraseña</label>
-									<input id="password" type="password" class="form-control" name="pass" required data-eye>
-								    <div class="invalid-feedback">
-								    	contraseña invalida
-							    	</div>
-								</div>
-
-								
-
-								<div class="form-group m-0">
-									<button type="submit" class="btn btn-primary btn-block">
-										Accesar
-									</button>
-								</div>
-								
-							</form>
-						</div>
-					</div>
-					<div class="footer">
-						Copyright &copy; 2013 &mdash; Amistad Bienes Raices 
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+ 
 
 
 
