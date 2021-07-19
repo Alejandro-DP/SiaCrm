2ca6e0d40cdbf072744e58e0669bd9fade34df88
Alejandro-DP <alexdelacruz1888@gmail.com>
2021-07-19 14:24:59 -0600

    correcion fondo

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="./css/bootstrap.min.css" rel="stylesheet" >
-    <link href="./css/styles.css" rel="stylesheet" >
+    <link href="./css/login-style.css" rel="stylesheet" >
     <title>SIA</title>
 </head>
 <body>
